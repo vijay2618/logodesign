061942db6c3853346cf27b7aee79f36401437e32
vijay <viju.2618@gmail.com>
2017-08-29 20:14:37 -0700

    WPFSimpleAPPlication

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Windows;
+
+namespace WpfSimple
+{
+    class MainWindowViewModel
+    {
+        private ICommand m_ButtonCommand;
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return m_ButtonCommand;
+            }
+            set
+            {
+                m_ButtonCommand = value;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            ButtonCommand=new RelayCommand(new Action<object>(ShowMessage));
+        }
+
+        public void ShowMessage(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+        }
+    }
+}
